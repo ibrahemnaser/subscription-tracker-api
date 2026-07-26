fa1fe21323572ab1b85c9d56b58df5de7707f533
ibrahmenaser <i.naser4040@gmail.com>
2026-07-26 19:09:00 +0300

    chore<app>: initialize JWT configuration

--- config/env.js
+++ config/env.js
@@ -4,4 +4,10 @@ config({
   path: `.env.${process.env.NODE_ENV || "development"}.local`,
 });
 
-export const { PORT, NODE_ENV = "development", DB_URI } = process.env;
+export const {
+  PORT,
+  NODE_ENV = "development",
+  DB_URI,
+  JWT_TOKEN,
+  JWT_EXPIRES_IN,
+} = process.env;
