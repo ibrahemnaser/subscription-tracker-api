b28688f9b2c75aba58f09e15e9d60ebfd64d8aeb
ibrahemnaser <i.naser4040@gmail.com>
2026-07-30 11:06:35 +0300

    update<user-route>: integrate controller for handling business logic

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
+// controllers
+import { getUser, getUsers } from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
-userRouter.get("/", (req, res) => res.send("Gett all users"));
-userRouter.get("/:id", (req, res) => res.send("Gett user by id"));
+userRouter.get("/", getUsers);
+userRouter.get("/:id", getUser);
 userRouter.post("/", (req, res) => res.send("create user"));
 userRouter.put("/:id", (req, res) => res.send("Update user"));
 userRouter.delete("/:id", (req, res) => res.send("Delete user"));
